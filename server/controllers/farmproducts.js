188c21d706f603462a0525b01dacb311b2214b9e
vihtle <vihtle2000@gmail.com>
2022-04-14 23:33:49 -0400

    some changes since i edited the db

--- server/controllers/farmproducts.js
+++ server/controllers/farmproducts.js
@@ -1,17 +1,17 @@
 const farmProductsGet = `SELECT farm_name, product_quantity, product_scale, product_name
-FROM \`urban farms\`
-JOIN \`farms products\` USING (farm_id)
-JOIN \`products\` USING (product_id);`
+FROM urban_farms
+JOIN farms_products USING (farm_id)
+JOIN products USING (product_id);`
 
-const farmProductsPut = `UPDATE \`farms products\`
+const farmProductsPut = `UPDATE farms_products
 SET product_quantity = :product_quantity, 
 product_scale = :product_scale, 
 WHERE farm_id = :farm_id AND product_id = :product_id;`
 
-const farmProductsPost = `INSERT INTO \`farms products\`(product_quantity, product_scale, farm_id, product_id)
+const farmProductsPost = `INSERT INTO farms_products(product_quantity, product_scale, farm_id, product_id)
 VALUES(:product_quantity, :product_scale, :farm_id, :product_id);`
 
-const farmProductsDelete = `DELETE FROM \`farms products\`
+const farmProductsDelete = `DELETE FROM farms_products
 WHERE farm_id = :farm_id AND product_id = :product_id;`
 
 export default {
