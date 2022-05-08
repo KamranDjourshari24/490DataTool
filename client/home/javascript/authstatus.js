0a5a7b60f6e10ff61191434e4355b28820a5d8ad
pool011 <jpool.school@gmail.com>
2022-05-08 19:27:31 -0400

    Icon lights up on active page (ugly code)

--- client/home/javascript/authstatus.js
+++ client/home/javascript/authstatus.js
@@ -2,17 +2,28 @@ function authHandler() {
   const farmName = sessionStorage.getItem('farmName');
   console.log(farmName);
   const loginBar = document.getElementById('loginBar');
+  let isLogin, isForm, isProfile = '';
+  if (window.location.href.includes('form.html')) {
+    isForm = ' active';
+  } else if (window.location.href.includes('profile')) {
+    isProfile = ' active';
+  } else if (window.location.href.includes('login')) {
+    isLogin = ' active';
+  }
+
   if (farmName == null | farmName == '') {
+
+
     loginBar.innerHTML = `
-    <li class="nav-item">
+    <li class="nav-item${isForm}">
       <a class="nav-link" href="../form/form.html" style="padding-left: 12px;padding-right: 12px;"><i class="fa fa-fw fa-file-text"></i> Register Your Local Farm Today </a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item${isLogin}">
       <a class="nav-link" href="../login/index.html" style="padding-left: 12px;padding-right: 12px;"><i class="fa fa-fw fa-user"></i> Login </a>
     </li>`
   } else {
     loginBar.innerHTML = `
-    <li class="nav-item">
+    <li class="nav-item${isProfile}">
       <a class="nav-link" href="../profile/index.html" style="padding-left: 12px;padding-right: 12px;"><i class="fa fa-fw fa-user"></i> Profile </a>
     </li>
     <li class="nav-item">
