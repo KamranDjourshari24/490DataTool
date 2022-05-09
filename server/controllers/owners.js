a42f2e53a50f978a0d80112fa2bab6aeb5fb99ef
pool011 <jpool.school@gmail.com>
2022-05-08 20:39:56 -0400

    owner queries updated

--- server/controllers/owners.js
+++ server/controllers/owners.js
@@ -1,9 +1,11 @@
-const ownersPost =`INSERT INTO owner (owner_id, fname, lname)
-VALUES(DEFAULT, :fname, :lname);`
+const ownersPost =`INSERT INTO owner (owner_id, fname, lname, username, password)
+VALUES(DEFAULT, :fname, :lname, :username, :password);`
 
 const ownersPut = `UPDATE owner
 SET fname = :fname, 
-lname = :lname
+lname = :lname,
+username = :username,
+password = :password
 WHERE owner_id = :owner_id;`
 
 const ownersDelete = `DELETE FROM owner
